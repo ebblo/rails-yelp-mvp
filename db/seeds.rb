a62d0e8417c55e5358c99b28779d41b73f621623
Loic Ebbing <ebbing.loic@outlook.com>
2020-08-12 17:25:25 +0200

    last save of the day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-restaurant = []
-restaurant << [{ name: 'Chez Loïc', address: 'Morrens', phone_number: '079 285 54 98', category: 'french' },
-{name: 'Chez Morgan', address: 'Morrens', phone_number: '079 285 54 98', category: 'belgian' },
-{name: 'Chez Johann', address: 'Renens', phone_number: '078 285 54 98', category: 'italian' },
-{name: 'Chez Redouane', address: 'Genève', phone_number: '077 285 54 98', category: 'chinese' },
-{name: 'Chez Jérémie', address: 'Saint-Denis', phone_number: '006 285 54 98', category: 'japanese' }]
+
+restaurants = Restaurant.create([{ name: 'Chez le Bel Homme François', address: 'Morrens', phone_number: '079 285 54 98', category: 'french' }, { name: 'Chez Morgan', address: 'Morrens', phone_number: '079 285 54 98', category: 'belgian' }, { name: 'Chez Johann', address: 'Renens', phone_number: '078 285 54 98', category: 'italian' }, {name: 'Chez Redouane', address: 'Genève', phone_number: '077 285 54 98', category: 'chinese' }, { name: 'Chez Jérémie', address: 'Saint-Denis', phone_number: '006 285 54 98', category: 'japanese' }])
